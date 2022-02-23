e6cfde5e029d5c9fc96c27f9f5b3c4973de88b36
lavanyaam1 <LAVANYA_A_M@mckinsey.com>
2022-02-23 20:24:13 +0530

    feat: revised addition of 2 test cases

--- test/airportDetails.test.js
+++ test/airportDetails.test.js
@@ -2,13 +2,24 @@ const airport = require('../airportDetails');
 
 describe("Airport Details",()=>{
     it("should throw an error if code length is greater than 3",()=>{
-        // const airportInfo = airport.airportInfo("AAAA");
-        expect(()=>{
-            airport.airportInfo("AAAA")
-        }).toThrow("Invalid Airport Code");
+        const airportInfo = airport.airportInfo("AAAA");
+        expect(airportInfo).toStrictEqual("Invalid Airport Code");
     });
+    it("should throw an error if airport code in not a string",()=>{
+        const airportInfo = airport.airportInfo(123);
+        expect(airportInfo).toStrictEqual("Invalid Airport Code");
+    });
+    it("should throw an error if the given code doesnot exits",()=>{
+        const airportInfo = airport.airportInfo("WER");
+        expect(airportInfo).toStrictEqual("Airport not found");
+    })
+        
     it("should return a list of airport name ,city and country ",()=>{
         const airportInfo = airport.airportInfo("AAA");
-        expect(airportInfo).toStrictEqual(['Anaa Airport', 'Anaa', 'French Polynesia'])
+        expect(airportInfo).toStrictEqual({name:'Anaa Airport', city:'Anaa', country:'French Polynesia'})
+    })
+    it("should return a list of airport name ,city and country ",()=>{
+        const airportInfo = airport.airportInfo("aaa");
+        expect(airportInfo).toStrictEqual({name:'Anaa Airport', city:'Anaa', country:'French Polynesia'})
     })
 })
